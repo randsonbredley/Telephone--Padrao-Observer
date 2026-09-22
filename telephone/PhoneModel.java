3c29e04423eae03104918fd2b659533b45e352e5
randsonbredley <randsonbredley@gmail.com>
2026-09-21 21:45:37 -0300

    PhoneModel passa a suportar e notificar observadores
    Atualizar o PhoneModel com registro de observadores e notificação ao adicionar dígitos.
    
    Ajuste realizado: Garanti que o modelo permaneça desacoplado da interface gráfica (não importa Screen nem usa System.out.println), respeitando a restrição de que o modelo conhece apenas a interface PhoneObserver

--- telephone/PhoneModel.java
+++ telephone/PhoneModel.java
@@ -6,9 +6,22 @@ import java.util.List;
  */
 public class PhoneModel {
     private List<Integer> digits = new ArrayList<>();
+    private List<PhoneObserver> observers = new ArrayList<>();
 
+    
+    public void addObserver(PhoneObserver observer) {
+        observers.add(observer);
+    }
+    
     public void addDigit(int newDigit) {
         digits.add(newDigit);
+        notifyObservers(newDigit);
+    }
+    
+    private void notifyObservers(int newDigit) {
+        for (PhoneObserver observer : observers) {
+            observer.digitAdded(newDigit);
+        }
     }
 
     public List<Integer> getDigits() {
