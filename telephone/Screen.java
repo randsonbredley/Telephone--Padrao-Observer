0ab02f617d924569fd05386ffdf12a96ff0157b6
randsonbredley <randsonbredley@gmail.com>
2026-09-21 21:49:14 -0300

    Screen cria e registra os dois observadores
    Implementar na UI os dois observadores para impressão do dígito recente e do número completo.
    
    Ajuste realizado: Optei por usar classes anônimas dentro de Screen para encapsular a lógica de exibição gráfica. Para o segundo observador, usei StringBuilder para concatenar a lista de inteiros retornada por model.getDigits() e adicionei o sulfixo "..." conforme a especificação do exemplo.

--- telephone/Screen.java
+++ telephone/Screen.java
@@ -1,3 +1,5 @@
+package telephone;
+
 /**
  * Prints things out to the screen, when needed
  * Printing to the screen:
@@ -8,5 +10,28 @@ public class Screen {
 
     public Screen(PhoneModel model) {
         this.model = model;
+        registerObservers();
+    }
+    
+    private void registerObservers() {
+        // Observador 1: Imprime o dígito mais recente
+        model.addObserver(new PhoneObserver() {
+            @Override
+            public void digitAdded(int digit) {
+                System.out.println(digit);
+            }
+        });
+
+        // Observador 2: Imprime o número completo acumulado
+        model.addObserver(new PhoneObserver() {
+            @Override
+            public void digitAdded(int digit) {
+                StringBuilder sb = new StringBuilder();
+                for (int d : model.getDigits()) {
+                    sb.append(d);
+                }
+                System.out.println("Agora discando " + sb.toString() + "...");
+            }
+        });
     }
 }
