a85cd3849cac6c9d882b21c529276ecd7b2889ba
randsonbredley <randsonbredley@gmail.com>
2026-09-21 21:43:32 -0300

    Adiciona interface PhoneObserver
    Solicitação para criar a interface do observador definindo qual parâmetro o método precisa receber.
    
    Ajuste realizado: Criei o método void digitAdded(int digit) passando apenas o dígito individual. Caso o observador precise da lista completa, ele pode consultar o próprio PhoneModel que já possui o método getDigits().

---
+++ telephone/PhoneModel.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Store a phone number, digit-by-digit
+ */
+public class PhoneModel {
+    private List<Integer> digits = new ArrayList<>();
+
+    public void addDigit(int newDigit) {
+        digits.add(newDigit);
+    }
+
+    public List<Integer> getDigits() {
+        return digits;
+    }
+}
